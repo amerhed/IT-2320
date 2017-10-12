251681ffaccecc085f52e0960294ed83a027ca64
amerhed <amy.hedrick18@gmail.com>
2017-10-11 20:33:09 -0400

    Unobtrusive Javascript
    Moved inline Javascript functions to their own file.

--- Assignment 1/scripts.js
+++ Assignment 1/scripts.js
@@ -1,23 +1,33 @@
-function Hello() {
-                alert("Hello! Welcome to my page!");
-            }
-
-document.getElementById("demo").innerHTML = Date();
-
-function myfunction(txt) { 
-    document.getElementById("demo-two").innerHTML = txt
-} 
-
-function myFunction() {
-    var greeting;
-    var time = new Date().getHours();
-    if (time < 12) {
-        greeting = "Good morning!";
-    } else if (time > 12) {
-        greeting = "Good afternoon!";
-    } else {
-        greeting = "Good evening!";
+window.onload = function load()
+{   
+    document.getElementById("message-button").onclick = function hello()
+    {
+      alert("Hello! Welcome to my page!"); 
     }
-document.getElementById("demo-three").innerHTML = greeting;
-}
+    
+    document.getElementById("demo").innerHTML = new Date();
 
+    document.getElementById("previewButton").onclick = function preview() 
+    { 
+        document.getElementById("demo-two").innerHTML = "Thanks for listening!"
+    } 
+    
+    document.getElementById("buyButton").onclick = function purchase() 
+    { 
+        document.getElementById("demo-two").innerHTML = "Thanks for your purchase!"
+    } 
+
+    document.getElementById("hello-button").onclick = function good() 
+    {
+        var greeting;
+        var time = new Date().getHours();
+        if (time < 12) {
+            greeting = "Good morning!";
+        } 
+        else if (time > 12) 
+        {
+            greeting = "Good afternoon!";
+        }
+    document.getElementById("greetingText").innerHTML = greeting;
+    }
+}
