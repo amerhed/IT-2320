f2d103e707de24adf7d1b89602e41f69e11be4db
amerhed <amy.hedrick18@gmail.com>
2017-10-15 20:20:35 -0400

    Uploading solved assignment 5

---
+++ Assignment 5/CheckerBoard/Scripts/Home.js
@@ -0,0 +1,47 @@
+﻿var isHoldingPiece = false;
+var pieceInHand = {};
+
+pickUpPiece = function (piece)
+{
+    $(piece).css("border", "1px solid pink");
+    isHoldingPiece = true;
+    pieceInHand = piece;
+}
+
+dropPiece = function (piece)
+{
+    piece.className = pieceInHand.className;
+    $(pieceInHand).css("border", "1px solid black")
+    pieceInHand.className = "cell";
+    isHoldingPiece = false;
+    pieceInHand = {};
+}
+
+
+$(document).ready(function ()
+{
+    var cells = $(".cell");
+    var colorCount = 0;
+
+    for (var i = 0; i < cells.length; i++)
+    {
+        var cell = $(cells[i]);
+        var isDark = colorCount % 2 == 0;
+        var isNextRow = (i + 1) % 8 == 0;
+        colorCount += isNextRow ? 2 : 1;
+        cell.css("background-color", isDark ? "navy" : "white");
+    }
+
+    $(".cell").click(function ()
+    {
+        if (!isHoldingPiece)
+        {
+            if ($(this).hasClass("piece")) {
+                pickUpPiece(this);
+            }
+        }
+        else {
+            dropPiece(this);
+        }
+    });
+});
