e27b6f640f2e909350baa2e9161ce925e45f704b
amerhed <amy.hedrick18@gmail.com>
2017-10-29 22:33:24 -0400

    included assignment 9

--- Assignment 1/scripts.js
+++ Assignment 1/scripts.js
@@ -31,3 +31,9 @@ window.onload = function load()
     document.getElementById("greetingText").innerHTML = greeting;
     }
 }
+
+ $(document).ready(function() {
+            $('#nav_menu').slicknav({
+                prependTo: "#mobile_menu"
+            });
+        });
