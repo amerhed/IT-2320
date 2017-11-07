3d2db86220273cfd7066c83d649ebb80fa99be6c
amerhed <amy.hedrick18@gmail.com>
2017-11-06 19:58:24 -0500

    start project week 10

---
+++ Week 10 - Final Project/FinalProject/Scripts/Home.js
@@ -0,0 +1,17 @@
+﻿var attempt = 3;
+
+function validate()
+{
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if (username == "Student" && password == "student123")
+    {
+        alert("Login Successful");
+        window.location = "accountinfo.html";
+        return false;
+    }
+    else
+    {
+        alert("Error: Invalid username or pasword.");
+    }
+}
