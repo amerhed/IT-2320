f1d964299c1dc3d22998e733e8553635233b3d1f
amerhed <amy.hedrick18@gmail.com>
2017-12-15 21:16:59 -0500

    Final Project Update

--- Week 10 - Final Project/FinalProject/Scripts/Home.js
+++ Week 10 - Final Project/FinalProject/Scripts/Home.js
@@ -14,4 +14,24 @@ function validate()
     {
         alert("Error: Invalid username or pasword.");
     }
+
+    
+}
+
+function validate_form()
+{
+    var username2 = document.getElementById("username2").value;
+    var password2 = document.getElementById("password2").value;
+    var email = document.getElementById("email").value;
+    var emailConfirm = document.getElementById("emailConfirm").value;
+
+    if ()
+    {
+        alert("Account Successfully Created");
+    }
+
+    else
+    {
+        alert("Error: Invalid Username, Password, or Email.");
+    }
 }
